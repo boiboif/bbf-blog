49d5634fb6811a0dc774698a3e10b198c93c0518
bbf <dg70946@163.com>
2022-09-30 22:50:34 +0800

    chore: 升级next版本到12.3.0

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,6 +4,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
         // this should be the actual path not a rewritten path
         // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+        await res.revalidate('/')
+        // await res.revalidate('/article')
         await res.revalidate(`/article/${req.query.id}`)
         return res.json({ revalidated: true })
     } catch (err) {
